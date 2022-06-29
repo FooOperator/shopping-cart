d886ed57f58a2f3e8c66b5a072538aff80d59c67
FooOperator <foxoperator1@gmail.com>
2022-06-29 22:26:06 +0100

    Moved to /shared

--- src/store/catalog/catalogSelectors.ts
+++
@@ -1,7 +0,0 @@
-import { createSelector } from "@reduxjs/toolkit";
-import { RootState } from "../store";
-
-export const catalogItemsSelector = createSelector(
-    (state: RootState) => state.catalog.items,
-    (items) => items
-);
--- src/utils/manipulations.ts
+++
@@ -1,4 +0,0 @@
-const currencyFormatter = new Intl.NumberFormat(undefined, { currency: 'USD', style: 'currency' });
-
-export const capitalize = (value: string) => value.charAt(0).toUpperCase() + value.slice(1);
-export const formatCurrency = (value: number) => currencyFormatter.format(value);
